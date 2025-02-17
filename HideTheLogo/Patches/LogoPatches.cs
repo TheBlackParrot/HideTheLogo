086df198e153d4ae4ea3fac0f05f8bb3fac3631a
TheBlackParrot <theblackparrot0@gmail.com>
2025-02-17 05:59:25 -0600

    Initial commit

---
+++ HideTheLogo/Patches/LogoPatches.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using HarmonyLib;
+using IPA.Utilities.Async;
+using UnityEngine;
+
+namespace HideTheLogo.Patches
+{
+    [HarmonyPatch]
+    internal class LogoPatches
+    {
+        private static Transform _defaultMenuEnvironment;
+        private static Transform _thunderMenuEnvironment;
+
+        private static async Task WaitABit()
+        {
+            // lazy. it works. w/e
+            await Task.Delay(1);
+        }
+        
+        // ReSharper disable once InconsistentNaming
+        [HarmonyPatch(typeof(MenuEnvironmentManager), "Start")]
+        [HarmonyPatch(typeof(MenuEnvironmentManager), "ShowEnvironmentType")]
+        [HarmonyPriority(int.MinValue)]
+        internal static void Postfix(MenuEnvironmentManager __instance)
+        {
+            if (_defaultMenuEnvironment == null)
+            {
+                _defaultMenuEnvironment = __instance.transform.Find("DefaultMenuEnvironment");
+            }
+            if (_thunderMenuEnvironment == null)
+            {
+                _thunderMenuEnvironment = __instance.transform.Find("ThunderMenuEnvironment");
+            }
+
+            UnityMainThreadTaskScheduler.Factory.StartNew(async () =>
+            {
+                Plugin.Log.Info("yeet");
+                
+                await WaitABit();
+                
+                _defaultMenuEnvironment.Find("Logo").gameObject.SetActive(false);
+                _defaultMenuEnvironment.Find("GlowLines").gameObject.SetActive(false);
+                _defaultMenuEnvironment.Find("GlowLines (1)").gameObject.SetActive(false);
+
+                _thunderMenuEnvironment.Find("MetallicaLogo").gameObject.SetActive(false);
+                _thunderMenuEnvironment.Find("BS_Logo").gameObject.SetActive(false);
+            });
+        }
+    }
+}
---
+++ HideTheLogo/Plugin.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using IPA;
+using JetBrains.Annotations;
+using SiraUtil.Zenject;
+using IPALogger = IPA.Logging.Logger;
+
+namespace HideTheLogo
+{
+    [Plugin(RuntimeOptions.DynamicInit), NoEnableDisable]
+    [UsedImplicitly]
+    public class Plugin
+    {
+        internal static IPALogger Log { get; private set; }
+        private static Harmony _harmony;
+
+        [Init]
+        [UsedImplicitly]
+        public void Init(Zenjector zenjector, IPALogger logger)
+        {
+            Log = logger;
+
+            zenjector.UseLogger(logger);
+            zenjector.UseMetadataBinder<Plugin>();
+        }
+        
+        [OnEnable]
+        [UsedImplicitly]
+        public void OnEnable()
+        {
+            _harmony = new Harmony("TheBlackParrot.HideTheLogo");
+            _harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [OnDisable]
+        [UsedImplicitly]
+        public void OnDisable()
+        {
+            _harmony.UnpatchSelf();
+        }
+    }
+}
